4fcf02f616b58d50ff7aa95b9af6f0328d50887b
Viet Danh Nguyen <vdnguyen3012@gmail.com>
2021-08-22 22:14:46 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running at 3000");
+});
+
+module.exports = app;
